48b49a42c2f403bf20658b235bae1882ad8711ea
imollov <ivomollov@gmail.com>
2020-05-09 12:21:03 +0300

    Setup prettier

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from 'react'
 
 function App() {
   return (
     <div>
       <p>Hello, world!</p>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
