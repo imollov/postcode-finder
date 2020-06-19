86cb8840ca864d238f8b602f4ba1f94646ab4af6
imollov <ivomollov@gmail.com>
2020-06-19 10:26:57 +0300

    Extract reusable functions

--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -1,18 +1,29 @@
+const hasPostalCode = (addressComponent) =>
+  addressComponent.types.includes('postal_code')
+
+const getPostalCode = (result) =>
+  result.address_components.find(hasPostalCode)['short_name']
+
+const transform = (result) => ({
+  address: result.formatted_address,
+  location: result.geometry.location,
+  postalCode: getPostalCode(result),
+})
+
 export default (state, action) => {
   switch (action.type) {
     case 'SEARCH':
       return {
         ...state,
-        suggestions: action.payload.map((s) => ({
-          address: s.formatted_address,
-          location: s.geometry.location,
-          id: s.place_id,
-        })),
+        suggestions: action.payload
+          .filter((s) => s.address_components.some(hasPostalCode))
+          .map(transform),
       }
     case 'SET_RESULT':
       return {
         ...state,
-        result: state.suggestions.filter((s) => s.address === s.payload),
+        result: state.suggestions.find((s) => s.address === action.payload),
+        suggestions: [],
       }
     case 'SEARCH_ERROR':
       return {
