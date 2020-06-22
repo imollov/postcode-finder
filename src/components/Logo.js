1c4e694e7e083d763b5fc567e6c917a12f4cde78
imollov <ivomollov@gmail.com>
2020-06-22 15:01:30 +0300

    Add emoji to logo and change cursor

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,4 +1,16 @@
 import React from 'react'
+import styled from 'styled-components'
 import { Heading } from 'grommet'
 
-export default (props) => <Heading {...props}>PostcodeFinder</Heading>
+const LogoHeading = styled(Heading)`
+  cursor: default;
+`
+
+export default (props) => (
+  <LogoHeading {...props}>
+    <span role="img" aria-label="Post office">
+      🏤
+    </span>
+    {' PostcodeFinder'}
+  </LogoHeading>
+)
