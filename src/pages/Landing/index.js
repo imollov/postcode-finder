1bf3efe1fc0762bf9cb2bc4c8e953954c20fe812
imollov <ivomollov@gmail.com>
2020-06-22 18:07:07 +0300

    Improve landing page responsiveness for small screens

--- src/pages/Landing/index.js
+++ src/pages/Landing/index.js
@@ -1,15 +1,26 @@
-import React from 'react'
-import { Box } from 'grommet'
+import React, { useContext } from 'react'
+import { Box, ResponsiveContext } from 'grommet'
+
 import Layout from './components/Layout'
 import Logo from '../../components/Logo'
 import AddressSearch from '../../components/AddressSearch'
 import LocationSearch from '../../components/LocationSearch'
 
 export default () => {
+  const size = useContext(ResponsiveContext)
+  const isSmall = size === 'small'
+
   return (
     <Layout>
       <Logo size="medium" color="accent" />
-      <Box direction="row" width="large" pad={{ horizontal: 'xlarge' }}>
+      <Box
+        direction="row"
+        width="large"
+        pad={{
+          horizontal: isSmall ? 'large' : 'xlarge',
+          vertical: isSmall ? 'medium' : 'xsmall',
+        }}
+      >
         <AddressSearch />
         <LocationSearch />
       </Box>
